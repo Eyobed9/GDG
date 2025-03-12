f0478f8e6f7783b9b6d1ff751a3a168f4b3f0535
Eyobed Demissie <127306098+Eyobed9@users.noreply.github.com>
2025-03-12 04:22:41 +0300

    Update Home.tsx

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -8,7 +8,7 @@ export const Home = () => {
 		<div>
 			<h1>{welcomeMessage}</h1>
 			<p className="mb-2">This is a website made for GDG project.</p>
-			<button className="p-2 bg-blue-600 rounded-md text-white" onClick={() => navigate("/contact")}>
+			<button className="p-2 bg-black-600 rounded-md text-white" onClick={() => navigate("/contact")}>
 				Contact us
 			</button>
 		</div>
